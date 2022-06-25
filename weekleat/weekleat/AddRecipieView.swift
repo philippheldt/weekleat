ba9085b8daddffa22219f7716d6410b3e03dc3a1
Philipp Jenny <register@philippjenny.de>
2022-06-25 11:03:47 +0200

    Adding multiple ingredients to one recipie

--- weekleat/weekleat/AddRecipieView.swift
+++ weekleat/weekleat/AddRecipieView.swift
@@ -14,30 +14,58 @@ struct AddRecipieView: View {
     @State private var title = ""
     @State private var portion = 5
     @State private var foodType = "pasta"
+    @State private var ingredients: [String] = []
+    @State private var ingredientSingle: String = ""
+    @State private var ingredientString: String = ""
     
     let foodTypes = ["enchilada", "pancake", "pasta", "pizza", "zucchini"]
     
     var body: some View {
         NavigationView{
             Form{
-                TextField("Rezeptname", text: $title)
-                Picker("Portionen", selection: $portion){
-                    ForEach(0..<10) {
-                        Text(String($0))
+                Section{
+                    TextField("Rezeptname", text: $title)
+                    Picker("Portionen", selection: $portion){
+                        ForEach(0..<10) {
+                            Text(String($0))
+                        }
+                    }
+                    Picker("Essensart", selection: $foodType){
+                        ForEach(foodTypes, id: \.self) {
+                            Text($0)
+                        }
                     }
                 }
-                Picker("Essensart", selection: $foodType){
-                    ForEach(foodTypes, id: \.self) {
-                        Text($0)
+                Section{
+                    List(ingredients, id:\.self) { ingredient in
+                        Text(ingredient)
+                        
+                    }
+                    HStack{
+                        TextField("Zutaten", text: $ingredientSingle)
+                        Button("Hinzufügen"){
+                            if ingredientSingle != ""{
+                                ingredients += ["\(ingredientSingle)"]
+                                ingredientSingle = ""
+                            }
+                        }
                     }
                 }
                 Section{
                     Button("Hinzufügen") {
                         let newRecipie = Recipie(context: moc)
-                            newRecipie.id = UUID()
-                            newRecipie.title = title
-                            newRecipie.portion = Int16(portion)
-                            newRecipie.foodType = foodType
+                        newRecipie.id = UUID()
+                        newRecipie.title = title
+                        newRecipie.portion = Int16(portion)
+                        newRecipie.foodType = foodType
+                        for ingredient in ingredients {
+                            if ingredientString != "" {
+                                ingredientString = "\(ingredientString),\(ingredient)"
+                            } else {
+                                ingredientString = ingredient
+                            }
+                        }
+                        print(ingredientString)
                         if foodType == "enchilada" || foodType == "pizza" {
                             newRecipie.colorTheme = 4
                         } else if foodType == "pasta" || foodType == "pancake" {
