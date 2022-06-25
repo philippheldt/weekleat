910707e1cbef88b1b6b99e3eb5093df6288710bd
Philipp Jenny <register@philippjenny.de>
2022-06-25 12:18:19 +0200

    Adding new Ingredients entity

--- weekleat/weekleat/AddRecipieView.swift
+++ weekleat/weekleat/AddRecipieView.swift
@@ -13,7 +13,9 @@ struct AddRecipieView: View {
     
     @State private var title = ""
     @State private var portion = 5
-    @State private var foodType = "pasta"
+   let step = 1
+    let range = 1...50
+    @State private var foodType = ""
     @State private var ingredients: [String] = []
     @State private var ingredientSingle: String = ""
     @State private var ingredientString: String = ""
@@ -25,16 +27,37 @@ struct AddRecipieView: View {
             Form{
                 Section{
                     TextField("Rezeptname", text: $title)
-                    Picker("Portionen", selection: $portion){
-                        ForEach(0..<10) {
-                            Text(String($0))
+                    Stepper(value: $portion, in: range, step: step) {
+                        HStack{
+                            Text("Portionen:")
+                            Spacer()
+                            Text("\(portion)")
                         }
                     }
-                    Picker("Essensart", selection: $foodType){
-                        ForEach(foodTypes, id: \.self) {
-                            Text($0)
+                    HStack{
+                        Text("Essensart")
+                        Spacer()
+                        Picker("Essensart", selection: $foodType){
+                            ForEach(foodTypes, id: \.self) {
+                                
+                                let first = String($0.prefix(1)).capitalized
+                                let other = String($0.dropFirst())
+                                let all = first + other
+                               
+                                HStack {
+                                    Image("pasta")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 1, height: 1)
+                                    Text(all)
+                                  
+                                }
+                                   
+                            }
                         }
+                        .pickerStyle(MenuPickerStyle())
                     }
+                    
                 }
                 Section{
                     List(ingredients, id:\.self) { ingredient in
@@ -43,11 +66,13 @@ struct AddRecipieView: View {
                     }
                     HStack{
                         TextField("Zutaten", text: $ingredientSingle)
-                        Button("Hinzufügen"){
+                        Button {
                             if ingredientSingle != ""{
                                 ingredients += ["\(ingredientSingle)"]
                                 ingredientSingle = ""
                             }
+                        } label: {
+                            Image(systemName: "plus.circle.fill")
                         }
                     }
                 }
--- weekleat/weekleat/CoreData/Datacontroller.swift
+++ weekleat/weekleat/CoreData/Datacontroller.swift
@@ -9,7 +9,7 @@ import CoreData
 import Foundation
 
 class DataController: ObservableObject {
-    let container = NSPersistentContainer(name: "Weekleat")
+    let container = NSPersistentContainer(name: "Recipies")
     
     init() {
         container.loadPersistentStores{ description, error in
