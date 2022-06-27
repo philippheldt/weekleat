02f8678ee46739805554393eda317ab0d01ac6c8
Philipp Jenny <register@philippjenny.de>
2022-06-27 12:23:39 +0200

    Making input-field smart to filter out Numbers and units

--- weekleat/weekleat/AddRecipieView.swift
+++ weekleat/weekleat/AddRecipieView.swift
@@ -17,8 +17,10 @@ struct AddRecipieView: View {
     let range = 1...50
     @State private var foodType = ""
     @State private var ingredients: [String] = []
-    @State private var ingredientSingle: String = ""
-    @State private var ingredientString: String = ""
+    @State private var ingredientsEntry: String = ""
+    @State private var amount: Int = 0
+    @State private var unit: String = ""
+    
     
     let foodTypes = ["enchilada", "pancake", "pasta", "pizza", "zucchini"]
     
@@ -63,11 +65,42 @@ struct AddRecipieView: View {
                         
                     }
                     HStack{
-                        TextField("Zutaten", text: $ingredientSingle)
+                        TextField("250g Mehl", text: $ingredientsEntry)
                         Button {
-                            if ingredientSingle != ""{
-                                ingredients += ["\(ingredientSingle)"]
-                                ingredientSingle = ""
+                            if ingredientsEntry != ""{
+                                //Creating array from Words in TextFioeld
+                                let ingrArr = ingredientsEntry.components(separatedBy: " ")
+                                
+                             
+                                for item in ingrArr{
+                                    //Filtering out the Numbers
+                                    let ingrArrNum = item.components(separatedBy: CharacterSet.decimalDigits.inverted)
+                                    var checkNum: Bool = false //If an array contains a Number, this will be updated
+                                    for numItem in ingrArrNum{
+                                        if let number = Int(numItem) {
+                                            amount = number
+                                            checkNum = true
+                                        }
+                                        
+                                    }
+                                    
+                                    if checkNum == true {
+                                        var unitDef: String = ""
+                                        let characters = Array(item) //Split up array position in single characters
+                                        for character in characters {
+                                            
+                                            if !character.isNumber{ //If its not a number it may be the unit, so its added to Unit
+                                                unitDef = "\(unitDef)\(character)"
+                                            }
+                                            
+                                        }
+                                        unit = unitDef
+                                    }
+                                    
+                                }
+                                
+                                ingredients += ["\(ingredientsEntry)"]
+                                ingredientsEntry = ""
                             }
                         } label: {
                             Image(systemName: "plus.circle.fill")
@@ -87,6 +120,8 @@ struct AddRecipieView: View {
                         for ingredient in ingredients {
                             let newIngredient = Ingredient(context: moc)
                             newIngredient.title = ingredient
+                            newIngredient.amount = Int16(amount)
+                            newIngredient.unit = unit
                             newRecipie.addToIngredients(newIngredient)
                         }
                         
